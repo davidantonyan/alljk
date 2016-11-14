17999bc4c75da2c7dabf6ae2e16d7b907030685f
David <davidantonyan1981@hotmail.com>
2016-11-15 01:33:56 +0400

    change config file

--- app/config.php
+++ app/config.php
@@ -2,12 +2,13 @@
 
 return [
 	'db' => [
-		'driver'   	 => "mysql",
-		'host'     	 => "127.0.0.1",
-		'username'   => "root",
-		'password'   => "",
-		'name'    	 => "alljk",
-		'charset'  	 => "utf8",
-		'collation'  => "utf8_unicode_ci",
+		'driver'=>'mysql',
+		'host'=>'127.0.0.1',
+		'username'=>'root',
+		'password'=> '',
+		'name'=>'alljk',
+		'charset'=>'utf8',
+		'collation'=>'utf8_unicode_ci',
 	],
 ];
+
